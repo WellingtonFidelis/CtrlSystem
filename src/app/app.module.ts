373937ec6b38bbd19a18ab1ae5a54db09785c663
WellingtonFidelis <wellingtonf20@gmail.com>
2019-08-03 19:26:34 -0300

    starting the routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,25 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { MaterializeModule } from 'angular2-materialize';
 
 import { AppComponent } from './app.component';
 
 import { FclrecebModule } from './fclreceb/fclreceb.module';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+   ],
   imports: [
     BrowserModule,
     FclrecebModule,
     FormsModule,
-    HttpClientModule,
+    HttpClientModule
     ],
   providers: [],
   bootstrap: [AppComponent]
